0ae3ebecf029c521d87985872916a0065789eb82
Michał Szczepanik <m.szczepanik@nencki.gov.pl>
2018-11-09 14:29:53 +0100

    Enable todo extension

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,7 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.todo',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -68,6 +69,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
 
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = True
 
 # -- Options for HTML output -------------------------------------------------
 
